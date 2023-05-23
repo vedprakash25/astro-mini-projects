dd545e56b4cceb5cb9ae0664557688138d3d291e
ved-blacksof <ved.blacksof@gmail.com>
2023-05-23 15:05:29 +0530

    push:astr-config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://vedprakash25.github.io/astro-mini-projects/",
   integrations: [
     tailwind({
       config: {
